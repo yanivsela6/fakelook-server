a63fcc15b3c538e6f96a4d2dd72a849f0f972dee
yanivsela6 <selayaniv6@gmail.com>
2022-03-20 20:44:00 +0200

    username

--- fakeLook-dal/Migrations/20220320184231_eraseusername.Designer.cs
+++ fakeLook-dal/Migrations/20220320184231_eraseusername.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fakeLook_dal.Data;
 
@@ -11,9 +12,10 @@ using fakeLook_dal.Data;
 namespace fakeLook_dal.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220320184231_eraseusername")]
+    partial class eraseusername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ fakeLook-dal/Migrations/20220320184231_eraseusername.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fakeLook_dal.Migrations
+{
+    public partial class eraseusername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
